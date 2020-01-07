55ad595cd476388948e0c09a39d725c9b24af5a5
BazMaster <bazilio100lei@gmail.com>
2020-01-07 12:24:49 +0200

    Фикс кэширования

--- 010_Caching/cache.js
+++ 010_Caching/cache.js
@@ -1,8 +1,8 @@
-module.exports {
+module.exports = {
     fname: "Ivan",
     lname: "Ivanov",
     age: 25,
     sayHello: function() {
         console.log('Hello! ', 'My name is ', this.fname, 'i\'m ', this.age, ' years old!');
     }
-};
+}
